b91247f12c023e6a8b91755836ca2a5560347ed3
238r1a05cn <238r1a05cn@cmrec.ac.in>
2024-11-12 10:39:10 +0530

    Create Main.java

---
+++ eb-dsa/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class main{
+  pubilc static void main(Strings[] args){
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    int y = sc.nextint();
+    if(x <= y){
+      System.out.println("YES");
+    }else{
+      System.out.println("NO");
+    }
+  }
+}
+

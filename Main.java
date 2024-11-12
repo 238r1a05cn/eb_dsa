c77692049399ed9214be86393999c9145f5c5202
238r1a05cn <238r1a05cn@cmrec.ac.in>
2024-11-12 10:30:33 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.utili.Scanner;
+class Main{
+  public static void static(Strings[] args){
+  Scanner sc = new Scanner(System.in);
+  int x = sc.nextInt();
+  int y = sc.nextInt();
+  if(x <= y){
+  System.out.println("YES");
+}else{
+  System.out.println("NO");
+}
+}
+}
